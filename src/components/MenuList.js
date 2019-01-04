0ed8e6bb4e25b12eab37a0c863dd487af06bd4c2
AJ Mcgladdery <amcgladd@gmail.com>
2019-01-04 14:25:40 -0800

    update menu prices and offerings

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -16,32 +16,62 @@ function MenuList(){
         <h1>Mains</h1>
           <div className="menu-spacing">
             <div className="menu-title-flex">
-              <h3>Burger</h3><h3> 10.00</h3>
+              <h3>Squeeze Burger</h3><h3> 8</h3>
             </div>
               <p>lettuce, tomato, pickles, onion, mayo, mustard</p>
           </div>
           <div className="menu-spacing">
             <div className="menu-title-flex">
-              <h3>Hotdog</h3><h3> 7.00</h3>
+              <h3>Squeeze With Cheese</h3><h3> 10</h3>
             </div>
-              <p>lettuce, tomatoes, pickles, onion, mayo, mustard, relish</p>
+              <p>lettuce, tomato, pickles, onion, mayo, mustard, cheese</p>
           </div>
           <div className="menu-spacing">
             <div className="menu-title-flex">
-              <h3>Chicken sandwich</h3><h3> 9.00</h3>
+              <h3>Cali Chicken Sandwich</h3><h3> 9</h3>
             </div>
-            <p>bacon, mayo, avocado, onion, lettuce, tomatoes</p>
+              <p>bacon, mayo, avocado, onion, lettuce, tomatoes</p>
+          </div>
+          <div className="menu-spacing">
+            <div className="menu-title-flex">
+              <h3>Veggie Burger</h3><h3> 9</h3>
+            </div>
+              <p>lettuce, tomato, pickles, onion, mayo, mustard, cheese</p>
+          </div>
+          <div className="menu-spacing">
+            <div className="menu-title-flex">
+              <h3>Hotdog</h3><h3> 8</h3>
+            </div>
+          </div>
+          <div className="menu-spacing">
+            <div className="menu-title-flex">
+              <h3>Grilled Cheese</h3><h3> 7</h3>
+            </div>
+          </div>
+          <div className="menu-spacing">
+            <div className="menu-title-flex">
+              <h3>Make It A Combo</h3><h3> +4</h3>
+            </div>
+              <p>includes your choice of side and drink</p>
           </div>
         </div>
         <div className="sides-menu">
           <h1>Sides</h1>
-          <div className="side-flex">
-          <div>
-          <p>fries</p>
-          <p>onion rings</p>
-          </div>
-            <p className="combo"><em>(sides included as combo with your main order)</em></p>
-          </div>
+            <div className="menu-spacing">
+              <div className="menu-title-flex">
+                <h3>Fries</h3><h3> 5</h3>
+              </div>
+            </div>
+            <div className="menu-spacing">
+              <div className="menu-title-flex">
+                <h3>Onion Rings</h3><h3> 5</h3>
+              </div>
+            </div>
+            <div className="menu-spacing">
+              <div className="menu-title-flex">
+                <h3>Drinks</h3><h3> 2</h3>
+              </div>
+            </div>
         </div>
       </div>
     </div>
